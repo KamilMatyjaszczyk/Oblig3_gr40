969d69d290bb2e45d62f52c01e01e94494d2548a
KamilMatyjaszczyk <Kamilmaty@hotmail.com>
2024-10-10 16:48:43 +0200

    initial commit

---
+++ src/main/java/com/spring/oblig3_gr40/Oblig3Gr40Application.java
@@ -0,0 +1,13 @@
+package com.spring.oblig3_gr40;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig3Gr40Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oblig3Gr40Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/oblig3_gr40/Oblig3Gr40ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.oblig3_gr40;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oblig3Gr40ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
